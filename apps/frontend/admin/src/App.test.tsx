a19499d5ba54928198276a76f6d40a4337220251
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-06-25 19:45:30 -0300

    feat(apps): :package: add admin app with cra typescript template

---
+++ apps/frontend/admin/src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('Renders the title', () => {
+    render(<App />);
+    const element = screen.getByText(/Admin CRA Application/i);
+    expect(element).toBeInTheDocument();
+});
