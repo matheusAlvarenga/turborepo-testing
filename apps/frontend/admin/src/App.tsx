615f3cfba1a585026fa032972fd2239bbaea4895
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-06-25 23:13:37 -0300

    refactor(apps/frontend/admin): :rotating_light: run eslint and prettier to fix code

--- apps/frontend/admin/src/App.tsx
+++ apps/frontend/admin/src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from "react";
 
-function App() {
-    return (
-        <div>
-            <h1>Admin CRA Application</h1>
-        </div>
-    );
-}
+const App = () => (
+  <div>
+    <h1>Admin CRA Application</h1>
+  </div>
+);
 
 export default App;
